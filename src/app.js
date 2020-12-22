722c19f46e6ee21f7c95007db0c2fd4e241bd969
vishal035 <kumarvishal880288@gmai;.com>
2020-12-22 09:10:55 +0530

    Logout Problem is fixed

--- src/app.js
+++ src/app.js
@@ -1,32 +1,29 @@
 const express = require('express');
 require('./db/mongoose');
-const userRouter = require('./routers/users')
-const taskRouter = require('./routers/tasks')
+const userRouter = require('./routers/users');
+const taskRouter = require('./routers/tasks');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // app.use((req,res,next) =>{
-    // console.log(req.method, req.path);
-    // if(req.method === 'GET'){
-    //     res.send("Get Request are disabled")
-    // }else{
-    //     next()
-    // }
+// console.log(req.method, req.path);
+// if(req.method === 'GET'){
+//     res.send("Get Request are disabled")
+// }else{
+//     next()
+// }
 
-    // res.status(503).send("Site Uner Mantanence") // --> To block all rquest
+// res.status(503).send("Site Uner Mantanence") // --> To block all rquest
 // })
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log(`Server is up on ${port}`);
-})
-
+  console.log(`Server is up on ${port}`);
+});
 
 // const jwt = require('jsonwebtoken')
 
