df9b667376d556c7268b24ebefb5a2ab26b9c430
vishal035 <kumarvishal880288@gmai;.com>
2021-04-08 12:24:01 +0530

    Testing User Seletion and Assertion in DB

--- test/user.test.js
+++ test/user.test.js
@@ -1,11 +1,21 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require('../src/index');
 const User = require('../src/models/users');
 
+const userOneId = new mongoose.Types.ObjectId();
+
 const userOne = {
+  _id: userOneId,
   name: 'Mike',
   email: 'somthing@anything.com',
   password: 'Yaaaa!!!!!',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWTSCERETLOCK),
+    },
+  ],
 };
 
 beforeEach(async () => {
@@ -15,7 +25,7 @@ beforeEach(async () => {
 });
 
 test('Should Signup a user', async () => {
-  await request(app)
+  const response = await request(app)
     .post('/users')
     .send({
       name: 'vishal Kumar',
@@ -24,6 +34,18 @@ test('Should Signup a user', async () => {
       password: 'red123!',
     })
     .expect(201);
+
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+  // console.log(user);
+  expect(response.body).toMatchObject({
+    user: {
+      name: 'vishal Kumar',
+      email: 'vishalkumar880288@gmail.com',
+    },
+    token: user.tokens[0].token,
+  });
+  expect(user.password).not.toBe('red123!');
 });
 
 // test('User Should Login', async () => {
@@ -42,4 +64,32 @@ test('Should Not Login', async () => {
     .post('/users/login')
     .send({ email: userOne.email, password: 'WRONG PASSWORD' })
     .expect(400);
+});
+
+test('Should Get Profile of Signed User', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('Shiould not get the profile without tokens', async () => {
+  await request(app).get('/users/me').send().expect(401);
+});
+
+test('Should Not Delete signed in user Account', async () => {
+  await request(app)
+    .delete('/users/me')
+    // .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(401);
+});
+
+test('Should Delete signed in user Account', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
 });
